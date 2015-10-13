c754d2e62ab604a60289cb2cbd3eab272a6e2344
Denis Nikolaev <idenisnikolaev@itsph.ru>
2015-10-13 23:41:53 +0300

    the first realization

--- src/ftpserver/FTPServer.java
+++ src/ftpserver/FTPServer.java
@@ -1,8 +1,16 @@
 
 package ftpserver;
 
-public class FTPServer {
-    public static void main(String[] args){
+import java.net.*;
+
+class FTPServer {
+    public static void main(String args[]) throws Exception {
+        ServerSocket soc = new ServerSocket(5217);
+        System.out.println("FTP Server Started on Port Number 5217");
         
+        while(true) {
+            System.out.println("Waiting for Connection ...");
+            TransferFile t = new TransferFile(soc.accept());   
+        }
     }
 }
