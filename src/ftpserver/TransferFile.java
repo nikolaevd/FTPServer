ca7710d15b5a11f450a490586dc83954e15facef
Denis Nikolaev <idenisnikolaev@gmail.com>
2015-10-14 15:54:29 +0300

    in process

--- src/ftpserver/TransferFile.java
+++ src/ftpserver/TransferFile.java
@@ -3,106 +3,133 @@ package ftpserver;
 
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 class TransferFile extends Thread {
-    Socket ClientSoc;
+    Socket incoming;
 
-    DataInputStream din;
-    DataOutputStream dout;
+    InputStream input;
+    OutputStream output;
     
     TransferFile(Socket soc) {
         try {
-            ClientSoc = soc;                        
-            din = new DataInputStream(ClientSoc.getInputStream());
-            dout = new DataOutputStream(ClientSoc.getOutputStream());
+            incoming = soc;                        
+            input = incoming.getInputStream();
+            output = incoming.getOutputStream();
             System.out.println("FTP Client Connected ...");
-            start();            
+            start();          
         }
         catch(Exception ex) {
             ex.printStackTrace();
         }        
     }
-    void SendFile() throws Exception {        
-        String filename = din.readUTF();
-        File f = new File(filename);
-        if(!f.exists()) {
-            dout.writeUTF("File Not Found");
-            return;
-        }
-        else {
-            dout.writeUTF("READY");
-            FileInputStream fin = new FileInputStream(f);
-            int ch;
-            do {
-                ch = fin.read();
-                dout.writeUTF(String.valueOf(ch));
+    
+    @Override
+    public void run(){
+        while(true){
+            try{
+                System.out.println("Waiting for command...");
+                try(Scanner sc = new Scanner(input)){
+                    PrintWriter out  = new PrintWriter(output, true);
+                    out.println("Hello! Enter BYE to exit.");
+                    
+                    boolean done = false;
+                    while(!done && sc.hasNextLine()){
+                        String line = sc.nextLine();
+                        out.println("Echo: " + line);
+                        if(line.trim().equals("BYE")) done = true;
+                    }
+                }
             }
-            while(ch != -1);    
-            fin.close();    
-            dout.writeUTF("File Receive Successfully");                            
+            catch(Exception ex){
+                
+            }
         }
     }
     
-    void ReceiveFile() throws Exception {
-        String filename = din.readUTF();
-        if(filename.compareTo("File not found") == 0) {
-            return;
-        }
-        File f = new File(filename);
-        String option;
-        
-        if(f.exists()) {
-            dout.writeUTF("File Already Exists");
-            option = din.readUTF();
-        }
-        else {
-            dout.writeUTF("SendFile");
-            option="Y";
-        }
-            
-        if(option.compareTo("Y") == 0) {
-            FileOutputStream fout = new FileOutputStream(f);
-            int ch;
-            String temp;
-            do {
-                temp = din.readUTF();
-                ch=Integer.parseInt(temp);
-                if(ch != -1) {
-                    fout.write(ch);                    
-                }
-            }
-            while(ch != -1);
-            fout.close();
-            dout.writeUTF("File Send Successfully");
-        }
-        else {
-            return;
-        }      
-    }
-
-    public void run() {
-        while(true) {
-            try {
-                System.out.println("Waiting for Command ...");
-                String Command = din.readUTF();
-                if(Command.compareTo("GET") == 0) {
-                    System.out.println("\tGET Command Received ...");
-                    SendFile();
-                    continue;
-                }
-                else if(Command.compareTo("SEND") == 0) {
-                    System.out.println("\tSEND Command Receiced ...");                
-                    ReceiveFile();
-                    continue;
-                }
-                else if(Command.compareTo("DISCONNECT") == 0) {
-                    System.out.println("\tDisconnect Command Received ...");
-                    System.exit(1);
-                }
-            }
-            catch(Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
+//    void SendFile() throws Exception {        
+//        String filename = input.readUTF();
+//        File f = new File(filename);
+//        if(!f.exists()) {
+//            output.writeUTF("File Not Found");
+//            return;
+//        }
+//        else {
+//            output.writeUTF("READY");
+//            FileInputStream fin = new FileInputStream(f);
+//            int ch;
+//            do {
+//                ch = fin.read();
+//                output.writeUTF(String.valueOf(ch));
+//            }
+//            while(ch != -1);    
+//            fin.close();    
+//            output.writeUTF("File Receive Successfully");                            
+//        }
+//    }
+//    
+//    void ReceiveFile() throws Exception {
+//        String filename = input.readUTF();
+//        if(filename.compareTo("File not found") == 0) {
+//            return;
+//        }
+//        File f = new File(filename);
+//        String option;
+//        
+//        if(f.exists()) {
+//            output.writeUTF("File Already Exists");
+//            option = input.readUTF();
+//        }
+//        else {
+//            output.writeUTF("SendFile");
+//            option="Y";
+//        }
+//            
+//        if(option.compareTo("Y") == 0) {
+//            FileOutputStream fout = new FileOutputStream(f);
+//            int ch;
+//            String temp;
+//            do {
+//                temp = input.readUTF();
+//                ch=Integer.parseInt(temp);
+//                if(ch != -1) {
+//                    fout.write(ch);                    
+//                }
+//            }
+//            while(ch != -1);
+//            fout.close();
+//            output.writeUTF("File Send Successfully");
+//        }
+//        else {
+//            return;
+//        }      
+//    }
+//
+//    @Override
+//    public void run() {
+//        while(true) {
+//            try {
+//                System.out.println("Waiting for Command ...");
+//                String Command = input.toString();
+//                System.out.println("JOPA");
+//                if(Command.compareTo("GET") == 0) {
+//                    System.out.println("\tGET Command Received ...");
+//                    SendFile();
+//                    continue;
+//                }
+//                else if(Command.compareTo("SEND") == 0) {
+//                    System.out.println("\tSEND Command Receiced ...");                
+//                    ReceiveFile();
+//                    continue;
+//                }
+//                else if(Command.compareTo("DISCONNECT") == 0) {
+//                    System.out.println("\tDisconnect Command Received ...");
+//                    System.exit(1);
+//                }
+//            }
+//            catch(Exception ex) {
+//                ex.printStackTrace();
+//            }
+//        }
+//    }
 }
