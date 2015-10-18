a0381d3e46c56374f147e71b2e4ee0a78faf8fab
Denis Nikolaev <idenisnikolaev@itsph.ru>
2015-10-18 17:37:23 +0300

    in process

--- src/ftpserver/Handling.java
+++ src/ftpserver/Handling.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class Handling {
     
     final int CONTROL_CONNECTION_PORT = 21;
+    final int DATA_CONNECTION_PORT = 20;
     
     public void createServer() throws IOException {
     
@@ -32,29 +33,66 @@ public class Handling {
                         
                         String line = in.nextLine();
                         out.println("> " + line);
-                        if(line.trim().equals("BYE")){
+                        
+                        String command = parseCommand(line);
+                        String argumnet = parseArgument(line);
+                        
+                        if(command.trim().equals("BYE")){
                             done = true;
                             out.println("> Connection closed.");
                         }
-                        else if(line.trim().equals("USER anonymous")){
+                        else if(command.trim().equals("USER") && argumnet.trim().equals("anonymous")){
                             out.println("230 User anonymous logged in.");
                         }
-                        else if(line.trim().equals("GET")){
+                        else if(command.trim().equals("GET")){
                             out.println("> GET Command Received...");
                         }
-                        else if(line.trim().equals("SEND")){
+                        else if(command.trim().equals("SEND")){
                             out.println("> SEND Command Received...");
                         }
-                        else if(line.trim().equals("PORT")){
+                        else if(command.trim().equals("EPRT")){
+                            out.println("> EPRT Command Received...");
+                            String[] test = erptHandler(argumnet);
+                        }
+                        else if(command.trim().equals("PORT")){
                             out.println("> PORT Command Received...");
                         }
                         else{
-                            out.println("Unrecognized command.");
+                            out.println("> Unrecognized command.");
                         }
                         
                     }
                 }
             }
         }
+    }
+    
+    private String parseCommand(String str){
+        String command = "";
+        
+        for(int i = 0; i < str.length(); i++){
+            if(str.substring(i, i+1).equals(" ")){
+                break;
+            }
+            command += str.substring(i, i+1);
+        }
+        return command;
+    }
+    
+    private String parseArgument(String str){
+        String argument = "";
+        
+        for(int i = 0; i < str.length(); i++){
+            if(str.substring(i, i+1).equals(" ")){
+                argument = str.substring(i+1);
+                break;
+            }        
+        }
+        return argument;
+    }
+    
+    private String[] erptHandler(String str){
+        String[] erpt = str.split("\\|");
+        return erpt; 
     }
 }
